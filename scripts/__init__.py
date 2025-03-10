121347f511bb45d984be5330aef75688efcfe47e
Erick Komati <kenzo.komati@gmail.com>
2025-03-09 22:10:55 -0300

    feat: adding the pdf files and the summary, structure fix

--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-class PDFReader:
-    def read(self, file_path):
-        with open(file_path, "rb") as f:
-            reader = PyPDF2.PdfReader(f)
-            text = "".join(page.extract_text() or "" for page in reader.pages)
-        return text
